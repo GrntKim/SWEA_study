fed99281ee7596f89668e0fc7bd3e7ef6444abbb
GrntKim <grantjun0224@gmail.com>
2026-05-29 18:30:15 +0900

    edit some code formatting of boilerplate

--- boilerplate/boilerplate.cpp
+++ boilerplate/boilerplate.cpp
@@ -6,7 +6,7 @@ int main(void) {
     ios::sync_with_stdio(false); cin.tie(nullptr);
     freopen("input.txt", "r", stdin);
     int T; cin >> T; 
-    for(int t=1; t<=T; t++) {
+    for (int t = 1; t <= T; t++) {
         int ans = 0;
         cout << '#' << t << ' ' << ans << '\n';
     }
